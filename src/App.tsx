dead3c26d1559e2fb785cc405f57ce114e793bd8
ChuckJoshuaa <chukwudi@emtech.com>
2026-02-23 10:33:48 +0100

    Initial First commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <main className="min-h-screen bg-white w-full">
       <HeroFeatures />
-      <div className="bg-[#F8F8F8]">
+      <div className="bg-[#F8F8F8]" style={{ marginTop: "2rem" }}>
         <Statistics />
       </div>
       <ProductHighlight />
